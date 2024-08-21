00382b3ea429111f95ea83b9beebc01a2b74ddd7
afthab <afthabvu@gmail.com>
2024-08-21 07:02:42 +0530

    Implemented Stripe checkout session with dynamic cart items and delivery options

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from './routes/myUserRoute';
 import myRestaurantRoute from './routes/myRestaurantRoute';
 import { v2 as cloudinary } from 'cloudinary';
 import restaurantRoute from './routes/RestaurantRoute';
+import orderRoute from './routes/OrderRoute';
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
@@ -31,6 +32,7 @@ app.get("/health",async(req:Request,res:Response)=>{
 app.use("/api/my/user",myUserRoute);
 app.use("/api/my/restaurant",myRestaurantRoute);
 app.use("/api/restaurant",restaurantRoute)
+app.use("/api/order",orderRoute)
 
 app.listen(7000,()=>{
     console.log("server started on PORT 7000")
