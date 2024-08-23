729ab78b4e033ef645d134058a8fef9b31e90587
afthab <afthabvu@gmail.com>
2024-08-23 06:47:43 +0530

    Stripe webhook functionality completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,39 +1,37 @@
-import express,{Request,Response} from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
-import mongoose from 'mongoose';
-import myUserRoute from './routes/myUserRoute';
-import myRestaurantRoute from './routes/myRestaurantRoute';
-import { v2 as cloudinary } from 'cloudinary';
-import restaurantRoute from './routes/RestaurantRoute';
-import orderRoute from './routes/OrderRoute';
+import mongoose from "mongoose";
+import myUserRoute from "./routes/myUserRoute";
+import myRestaurantRoute from "./routes/myRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
+import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
-
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
-    console.log('connected to mongodb')
-})
-
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+  console.log("connected to mongodb");
+});
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const app =express();
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(cors());
 
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+app.use(express.json());
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
-app.get("/health",async(req:Request,res:Response)=>{
-    res.send({message:"health OK!"})
-})
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
-app.use("/api/my/user",myUserRoute);
-app.use("/api/my/restaurant",myRestaurantRoute);
-app.use("/api/restaurant",restaurantRoute)
-app.use("/api/order",orderRoute)
-
-app.listen(7000,()=>{
-    console.log("server started on PORT 7000")
-})
+app.listen(7000, () => {
+  console.log("server started on PORT 7000");
+});
